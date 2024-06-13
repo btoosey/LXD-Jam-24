e595d2b3bf065cc02ffd780d139619c3c9ef286e
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-06-13 20:13:59 +0100

    UPDATE: Final QoL changes

--- scenes/game_states/game_start_state.gd
+++ scenes/game_states/game_start_state.gd
@@ -2,6 +2,7 @@ extends GameState
 @onready var camera_3d = $"../../Camera3D"
 @onready var title = $"../../UI/Title"
 @onready var start_button = $"../../UI/StartButton"
+@onready var subtitle = $"../../UI/Subtitle"
 
 func exit():
 	var camera_tween = get_tree().create_tween()
@@ -12,3 +13,4 @@ func exit():
 	
 	start_button.disabled = true
 	start_button.visible = false
+	subtitle.visible = false
