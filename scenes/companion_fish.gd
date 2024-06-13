173c5357ef357891db9f4b9418e13000a4d6926d
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-06-13 11:24:21 +0100

    ADD: Random movement for companion
    Basic random movement implemented. Still needs refining

--- scenes/companion_fish.gd
+++ scenes/companion_fish.gd
@@ -1,8 +1,9 @@
 extends CharacterBody3D
 
 var target_position: Vector3
-const SPEED: float = 5
+const SPEED: float = 7
 @onready var timer = $Timer
+@onready var target_position_raycast = $TargetPositionRaycast
 
 func _ready():
 	choose_target()
@@ -16,8 +17,20 @@ func _physics_process(_delta):
 	velocity = direction * SPEED
 	move_and_slide()
 
+	if position.distance_to(target_position) <= 0.1:
+		target_position = position
+
 func choose_target() -> void:
-	print("target")
+
+	var rand_direction = Vector2(randf_range(-1,1),randf_range(-1,1)).normalized() * 10
+
+	var temp_target_position = position + Vector3(rand_direction.x, 0, rand_direction.y)
+
+	target_position_raycast.position = temp_target_position
+	target_position_raycast.target_position = temp_target_position + Vector3(0, -5, 0)
+	target_position_raycast.debug_shape_custom_color = Color.BLUE
+	target_position = temp_target_position
+	look_at(target_position, Vector3.UP)
 
 func _on_timer_timeout():
 	choose_target()
